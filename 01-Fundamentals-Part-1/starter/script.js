200789e801bfbb5b7bf76888af0e8700cbb48457
Zhiren Zhou <zzr997good@tamu.edu>
2023-03-23 16:39:38 -0500

    code_challenging_1

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,14 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
+// let js = 'amazing';
+// if (js === 'amazing') alert('JavaScript is FUN!');
 
-40 + 8 + 23 - 10;
-console.log(40 + 8 + 23 - 10);
+// 40 + 8 + 23 - 10;
+// console.log(40 + 8 + 23 - 10);
+
+// let markMass = 78, markHeight = 1.69;
+// let johnMass = 92, johnHeight = 1.95;
+let markMass = 95, markHeight = 1.88;
+let johnMass = 85, johnHeight = 1.76;
+let markBMI = markMass / (markHeight ** 2), johnBMI = johnMass / (johnHeight ** 2);
+console.log(markBMI, johnBMI);
+let markHigherBMI = markBMI > johnBMI;
+console.log(markHigherBMI);
