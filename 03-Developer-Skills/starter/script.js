af24884c79a8fdee7b1ff45b2eb4c0355fa72636
Zhiren Zhou <zzr997good@tamu.edu>
2023-04-13 15:53:33 -0500

    04/11/2023

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const printForecast = function (arr) {
+  let res = '';
+  for (let i = 0; i < arr.length; i++) {
+    res += `... ${arr[i]} \u2103 in ${i + 1} days `;
+  }
+  return res;
+};
+
+console.log(printForecast([17, 21, 23]));
+console.log(printForecast([12, 5, -5, 0, 4]));
+// console.log('hello');
